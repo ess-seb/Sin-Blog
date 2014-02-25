467e057a8bf6fc994de1307d3f554c3edd5eb216
Sebastian <ess.seb@gmail.com>
2014-02-25 15:33:30 +0100

    drugie wwarsztaty

--- hi.rb
+++ hi.rb
@@ -3,7 +3,7 @@ require 'sinatra/reloader'
 require 'pry'
 require 'haml'
 require 'data_mapper'
-
+require 'dm-sqlite-adapter'
 
 # get '/hi' do
 #   "Hello World!"
@@ -32,3 +32,46 @@ get '/viewerb' do
 	@items = ["yo", "jo"]
   erb :viewerb, :locals => {:items => @items}
 end
+
+get '/posts/new' do 
+	erb :view_newpost, lay
+end
+
+post '/posts/create' do 
+		# binding.pry params
+	
+		# my_post = Post.new
+
+		# my_post.attributes = params
+		# my_post.save
+		Post.create(:title => params[:title], :body => params[:content])
+end
+
+get '/posts' do
+		@post_list = Post.all
+		erb :posts_list 
+end
+
+
+
+require 'rubygems'
+require 'sinatra'
+require 'data_mapper' # metagem, requires common plugins too.
+
+# need install dm-sqlite-adapter
+DataMapper::setup(:default, "sqlite3://#{Dir.pwd}/blog.db")
+
+class Post
+    include DataMapper::Resource
+    property :id, Serial
+    property :title, String
+    property :body, Text
+    property :created_at, DateTime
+end
+
+# Perform basic sanity checks and initialize all relationships
+# Call this when you've defined all your models
+DataMapper.finalize
+
+# automatically create the post table
+Post.auto_upgrade!
